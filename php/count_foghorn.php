fd466b2fed6285c9de5225ad37007564db5d40ad
Rodney Ritchey <rodneyritchey@gmail.com>
2013-11-23 13:18:42 -0500

    Foghorn Commit
    Set count_foghorn.php

--- php/count_foghorn.php
+++ php/count_foghorn.php
@@ -2,16 +2,13 @@
 
 require_once ('db_connect.php');
 
-$sql="SELECT * FROM Persons WHERE vote='Foghorn'";
+$sql="SELECT * FROM Persons WHERE vote='f'";
 
 if ($result=mysqli_query($con,$sql))
   {
   // Return the number of rows in result set
-  $rowcount=mysqli_num_rows($result);
-  echo "<div class='result_can'>";
-  echo "<img src='img/foghorn01.png' />";
-  echo "<h2>Foghorn has " .$rowcount. " votes!</h2>";
-  echo "</div>";
+  $rowcountf=mysqli_num_rows($result);
+  //printf("Result set has %d rows.\n",$rowcount);
   // Free result set
   mysqli_free_result($result);
   }
